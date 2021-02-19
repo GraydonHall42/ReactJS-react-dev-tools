552193886a631b295bd5820e57e8d91c5e6588f8
GraydonHall42 <43125094+GraydonHall42@users.noreply.github.com>
2021-02-19 12:53:42 -0700

    finished module on react dev tool in chrome also discussed the idea of having a palette of components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=980:*" />
 
       <Card
         name={contacts[0].name}
